72db66cdb5b28548bb78fab8ecfe2e84ba066699
yunsusu <rladbstn1212@gmail.com>
2024-08-30 23:39:27 +0900

    fix : 타입에러로 빌드 에러나는거 수정

--- src/pages/location/index.tsx
+++ src/pages/location/index.tsx
@@ -1,27 +1,29 @@
 import { getLocation } from "@/lib/apis/geo";
-import { getPlace, getPlaceNameByOSM } from "@/lib/apis/location";
+import { getPlaceNameByOSM } from "@/lib/apis/location";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 function Location() {
-  const [xy, setXy] = useState({});
+  const [xy, setXy] = useState<any>(null);
 
   const { data: locationData } = useQuery({
     queryKey: ["locations"],
-    queryFn: getLocation, // 함수 참조로 넘겨야 함
+    queryFn: getLocation,
   });
+
   const { data: place, isLoading } = useQuery({
-    queryKey: ["place", xy],
-    queryFn: () => getPlaceNameByOSM(xy.x, xy.y), // 함수 참조로 넘겨야 함
-    enabled: !!xy,
+    queryKey: ["place"],
+    queryFn: () => getPlaceNameByOSM(xy.x, xy.y),
+    enabled: xy !== null,
   });
-  console.log(place);
+
   useEffect(() => {
-    if (locationData) {
+    if (locationData && Array.isArray(locationData)) {
       setXy(locationData[0]);
     }
   }, [locationData]);
-  return <div>{isLoading ? "로딩중.." : place}</div>;
+
+  return <div>{isLoading ? "로딩중.." : place || "장소 정보 없음"}</div>;
 }
 
 export default Location;
