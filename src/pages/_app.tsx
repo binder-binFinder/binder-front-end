58d5f28804df0292dfde2fa254bc1f245dd0a3f0
yunsusu <rladbstn1212@gmail.com>
2024-09-01 21:29:17 +0900

    feat : gnb 구현현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import "@/styles/base/index.scss";
+import Gnb from "@/components/commons/Gnb";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Gnb />
     </QueryClientProvider>
   );
 }
