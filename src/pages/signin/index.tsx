baaebefbedfc22c6ccf5c0f1e3571871e89405f9
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-09-03 15:15:14 +0900

    feat : toast 컴포넌트 구현

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,47 +1,32 @@
 import SocialoginButton from "@/components/domains/signin/SocialoginButton";
 import styles from "./signin.module.scss";
 import classNames from "classnames/bind";
-import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getMembersMe } from "@/lib/apis/getMembersMe";
-import { useAtom } from "jotai";
-import { userAtom } from "@/lib/atoms/userAtom";
+import { SOCIAL_LOGIN_URL } from "@/lib/constants/urls";
+import Toast from "@/components/domains/signin/Toast";
 
 const cn = classNames.bind(styles);
 
 export default function Signin() {
-  const [user, setUser] = useAtom(userAtom);
-
-  // const { data: userData, isLoading, isError } = useQuery({ queryKey: ["user"], queryFn: () => getMembersMe() });
-
-  // useEffect(() => {
-  //   if (userData && !user) {
-  //     setUser(userData);
-  //     console.log(userData);
-  //   }
-  // }, [userData, user, setUser]);
-
-  //.env 에 담아서 url 변수로 지정하기 -- 나중에
   const handleClickKakaoSignIn = () => {
-    window.location.href = "https://api.bin-finder.net/oauth2/authorization/kakao";
+    window.location.href = SOCIAL_LOGIN_URL + "/kakao";
   };
 
   const handleClickNaverSignIn = () => {
-    window.location.href = "https://api.bin-finder.net/oauth2/authorization/naver";
+    window.location.href = SOCIAL_LOGIN_URL + "/naver";
   };
 
-
   const handleClickGoogleSignIn = () => {
-    window.location.href = "https://api.bin-finder.net/oauth2/authorization/google";
+    window.location.href = SOCIAL_LOGIN_URL + "/google";
   };
 
   return (
-    <>
+    <section className={cn("wrapper")}>
       <article className={cn("btns-wrapper")}>
+        <Toast />
         <SocialoginButton label="kakao" onClick={handleClickKakaoSignIn} testId={"kakaoBtn"} />
         <SocialoginButton label="naver" onClick={handleClickNaverSignIn} testId={"naverBtn"} />
         <SocialoginButton label="google" onClick={handleClickGoogleSignIn} testId={"googleBtn"} />
       </article>
-    </>
+    </section>
   );
 }
