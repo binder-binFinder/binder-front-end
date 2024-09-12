fd1ea98d17bfe2fece483582aaa06b5e703dbbbb
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-09-13 00:34:19 +0900

    feat : 피그마 상의 모든 모달 구현

--- src/components/domains/mypage/Admin/AdminDetail.tsx
+++ src/components/domains/mypage/Admin/AdminDetail.tsx
@@ -1,8 +1,6 @@
 import classNames from "classnames/bind";
-
 import Image from "next/image";
 import styles from "./AdminDetail.module.scss";
-
 import { getBinsId } from "@/lib/apis/bins";
 import { btnInputValues } from "@/lib/constants/btnInputValues";
 import { useQuery } from "@tanstack/react-query";
@@ -10,6 +8,11 @@ import { useRouter } from "next/router";
 import AdminDetailItem from "./AdminDetailItem";
 
 const cn = classNames.bind(styles);
+
+interface Props {
+  state: "등록" | "신고" | "수정";
+  approve: () => void;
+}
 
 export default function DefaultForm({ state, approve }: any) {
   const router = useRouter();
@@ -21,56 +24,42 @@ export default function DefaultForm({ state, approve }: any) {
 
   return (
     <>
-      <div className={cn("detailWrap")}>
-        <div className={cn("detailTitle")}>쓰레기통 {state} 심사</div>
-      </div>
-      <div className={cn("detailInner")}>
+      <article className={cn("detailWrap")}>
+        <h3 className={cn("detailTitle")}>쓰레기통 {state} 심사</h3>
+      </article>
+      <section className={cn("detailInner")}>
         <AdminDetailItem title={"쓰레기통 주소"} detail={binDetail?.address} />
 
-        <div className={cn("detail")}>
-          <div>쓰레기통 분류</div>
+        <article className={cn("detail")}>
+          <h4>쓰레기통 분류</h4>
           <div className={cn("detailSelect")}>
             {btnInputValues.map((item, index) => (
               <div
                 key={index}
-                className={
-                  binDetail?.type === item.id
-                    ? cn("detailSelectItemOn")
-                    : cn("detailSelectItem")
-                }
+                className={binDetail?.type === item.id ? cn("detailSelectItemOn") : cn("detailSelectItem")}
               >
                 {item.label}
               </div>
             ))}
           </div>
-        </div>
+        </article>
         <AdminDetailItem title={"장소 명칭"} detail={binDetail?.title} />
         {binDetail?.imageUrl && binDetail?.imageUrl !== "string" && (
-          <div className={cn("detail")}>
-            <div>
+          <article className={cn("detail")}>
+            <h4>
               쓰레기통 사진<span>(선택)</span>
-            </div>
-            <Image
-              src={binDetail?.imageUrl}
-              alt="이미지"
-              width={356}
-              height={143}
-            />
-          </div>
+            </h4>
+            <Image src={binDetail?.imageUrl} alt="이미지" width={356} height={143} />
+          </article>
         )}
-        {state === "수정" && (
-          <AdminDetailItem
-            title={"수정 요청 사유"}
-            detail={"binDetail?.title"}
-          />
-        )}
-        <div className={cn("detailBtn")}>
-          <div className={cn("detailReject")}>등록 거절</div>
-          <div className={cn("detailAccept")} onClick={approve}>
-            등록 승인
-          </div>
-        </div>
-      </div>
+        {state === "수정" && <AdminDetailItem title={"수정 요청 사유"} detail={"binDetail?.title"} />}
+        <article className={cn("detailBtn")}>
+          <button className={cn("detailReject")}>{state} 거절</button>
+          <button className={cn("detailAccept")} onClick={approve}>
+            {state} 승인
+          </button>
+        </article>
+      </section>
     </>
   );
 }
--- src/pages/mypage/fix/[id].tsx
+++ src/pages/mypage/fix/[id].tsx
@@ -1,12 +1,15 @@
+import Modal from "@/components/commons/Modal/TrashHow";
 import AdminDetail from "@/components/domains/mypage/Admin/AdminDetail";
 import AdminPageBar from "@/components/domains/mypage/Admin/AdminPageBar";
 import { postAcceptFix } from "@/lib/apis/fix";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function AskDetail() {
   const router = useRouter();
   const { id } = router.query;
+  const [isOpenModal, setIsOpenModal] = useState(true);
 
   const { mutate: handleAccept } = useMutation({
     mutationFn: () => postAcceptFix(id),
@@ -15,10 +18,15 @@ export default function AskDetail() {
     },
   });
 
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
     <>
       <AdminPageBar />
       <AdminDetail state={"수정"} approve={handleAccept} />
+      {isOpenModal && <Modal modalClose={handleCloseModal} />}
     </>
   );
 }
