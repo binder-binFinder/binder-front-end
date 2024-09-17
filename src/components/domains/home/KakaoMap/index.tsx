34f92018dc9d6248315d112b37f569ddc3eb4250
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-09-17 22:53:18 +0900

    feat :  map center 이동 시 500 미터 이내 등록 된쓰레기통 불러오기

--- src/components/domains/home/KakaoMap/index.tsx
+++ src/components/domains/home/KakaoMap/index.tsx
@@ -1,6 +1,10 @@
 import { getLocation } from "@/lib/apis/geo";
 import { getAroundbins } from "@/lib/apis/search";
-import { userAddress, userCoordinate } from "@/lib/atoms/userAtom";
+import {
+  mapCenterCoordinate,
+  userAddress,
+  userCoordinate,
+} from "@/lib/atoms/userAtom";
 import {
   addMyLocationMarker,
   createMarker,
@@ -8,11 +12,11 @@ import {
   getMarkerImage,
   initMap,
 } from "@/lib/map";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import classNames from "classnames/bind";
 import { useAtom } from "jotai";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./KakaoMap.module.scss";
 
 const cn = classNames.bind(styles);
@@ -22,6 +26,64 @@ declare global {
     kakao: any;
   }
 }
+function debounce(func: (...args: any[]) => void, delay: number) {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+const loadKakaoMapScript = (callback: () => void) => {
+  if (window.kakao && window.kakao.maps) {
+    callback();
+  } else {
+    const kakaoMapScript = document.createElement("script");
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services,clusterer,drawing&autoload=false`;
+    kakaoMapScript.async = true;
+    kakaoMapScript.onload = callback;
+    document.head.appendChild(kakaoMapScript);
+  }
+};
+
+const initializeMap = (coordinate: any, setAddress: any) => {
+  if (coordinate.x !== 0 && coordinate.y !== 0) {
+    const map = initMap(window.kakao, coordinate);
+    const myLocationMarker = addMyLocationMarker(map, window.kakao, coordinate);
+
+    getAddressFromCoords(window.kakao, coordinate, (getAddress: any) => {
+      setAddress({
+        roadAddress: getAddress.road_address?.address_name || null,
+        address: getAddress.address?.address_name,
+      });
+    });
+
+    return { map, myLocationMarker };
+  }
+  return null;
+};
+
+const updateMarkers = (binData: any, map: any, binkMarkerRef: any) => {
+  if (binkMarkerRef.current.length > 0) {
+    binkMarkerRef.current.forEach((marker: any) => marker.setMap(null));
+  }
+
+  if (binData && map) {
+    const newMarkers = binData.map((bin: any) => {
+      const marker = createMarker(
+        window.kakao,
+        map,
+        { x: bin.latitude, y: bin.longitude },
+        getMarkerImage(bin.isBookMarked, bin.type),
+        bin.title
+      );
+      return marker;
+    });
+    binkMarkerRef.current = newMarkers;
+  }
+};
 
 export default function KakaoMap() {
   const [coordinate, setCoordinate] = useAtom(userCoordinate);
@@ -29,8 +91,7 @@ export default function KakaoMap() {
   const mapRef = useRef<any>(null);
   const myMarkerRef = useRef<any>(null);
   const binkMarkerRef = useRef<any>([]);
-  const queryClient = useQueryClient();
-  const [centerCoordinate, setCenterCoordinate] = useState(coordinate); // 지도의 중심 좌표 따로 관리
+  const [centerCoordinate, setCenterCoordinate] = useAtom(mapCenterCoordinate);
 
   const { data: locationData, refetch: locationRefetch } = useQuery<any>({
     queryKey: ["locations"],
@@ -39,109 +100,48 @@ export default function KakaoMap() {
   });
 
   const { data: binData, refetch: refetchBinData } = useQuery({
-    queryKey: ["get-around-bins", centerCoordinate], // 지도의 중심 좌표를 사용
+    queryKey: ["get-around-bins", centerCoordinate],
     queryFn: () =>
       getAroundbins(centerCoordinate.x, centerCoordinate.y, null, 500),
-    enabled: centerCoordinate.x !== 0,
+    enabled: !!centerCoordinate.x && !!centerCoordinate.y,
+    gcTime: 3000,
   });
 
-  // 좌표 데이터 업데이트
-  useEffect(() => {
-    if (locationData && Array.isArray(locationData)) {
-      setCoordinate(locationData[0]);
-    }
-  }, [locationData]);
-
-  // Kakao 지도 스크립트 로드 및 지도 초기화
-  useEffect(() => {
-    const loadKakaoMap = () => {
-      if (window.kakao && window.kakao.maps) {
-        window.kakao.maps.load(() => {
-          if (coordinate.x !== 0 && coordinate.y !== 0) {
-            const map = initMap(window.kakao, coordinate);
-            const myLocationMarker = addMyLocationMarker(
-              map,
-              window.kakao,
-              coordinate
-            );
-
-            mapRef.current = map;
-            myMarkerRef.current = myLocationMarker;
-
-            getAddressFromCoords(
-              window.kakao,
-              coordinate,
-              (getAddress: any) => {
-                setAddress({
-                  roadAddress: getAddress.road_address?.address_name || null,
-                  address: getAddress.address?.address_name,
-                });
-              }
-            );
-            if (binkMarkerRef.current.length > 0) {
-              binkMarkerRef.current.forEach((marker: any) =>
-                marker.setMap(null)
-              );
-            }
-
-            // 새로운 마커 추가
-            if (binData) {
-              const newMarkers = binData.map((bin: any) => {
-                const marker = createMarker(
-                  window.kakao,
-                  mapRef.current,
-                  { x: bin.latitude, y: bin.longitude },
-                  getMarkerImage(bin.isBookMarked, bin.type),
-                  bin.title
-                );
-                return marker;
-              });
-
-              binkMarkerRef.current = newMarkers;
-            }
-
-            // const center = map.getCenter();
-
-            // if (
-            //   center.getLat() !== coordinate.x ||
-            //   center.getLng() !== coordinate.y
-            // ) {
-            //   const newCenterCoordinate = {
-            //     x: center.getLat(),
-            //     y: center.getLng(),
-            //   };
-
-            //   // 중심 좌표 업데이트
-            //   setCenterCoordinate(newCenterCoordinate);
-            //   // 새로운 중심 좌표로 binData 다시 요청
-            //   refetchBinData();
-            // }
-          }
-        });
-      }
+  const debouncedHandleCenterChanged = debounce(() => {
+    const center = mapRef.current.getCenter();
+    const newCenterCoordinate = {
+      x: center.getLat(),
+      y: center.getLng(),
     };
+    setCenterCoordinate(newCenterCoordinate);
+    console.log("center", center);
+  }, 500);
 
-    // Kakao Map 스크립트가 이미 로드되었는지 확인
-    if (window.kakao && window.kakao.maps) {
-      loadKakaoMap();
-    } else {
-      const kakaoMapScript = document.createElement("script");
-      kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services,clusterer,drawing&autoload=false`;
-      kakaoMapScript.async = true;
-      kakaoMapScript.onload = loadKakaoMap;
-      document.head.appendChild(kakaoMapScript);
-    }
+  useEffect(() => {
+    loadKakaoMapScript(() => {
+      const result = initializeMap(coordinate, setAddress);
+      if (result) {
+        mapRef.current = result.map;
+        myMarkerRef.current = result.myLocationMarker;
+
+        updateMarkers(binData, mapRef.current, binkMarkerRef);
+
+        window.kakao.maps.event.addListener(
+          mapRef.current,
+          "center_changed",
+          debouncedHandleCenterChanged
+        );
+      }
+    });
 
     return () => {
       if (mapRef.current) {
-        // 기존 마커나 지도 상태 초기화 (필요 시)
         binkMarkerRef.current.forEach((marker: any) => marker.setMap(null));
         binkMarkerRef.current = [];
       }
     };
-  }, [coordinate, binData]);
+  }, [coordinate]);
 
-  // 사용자가 위치 버튼 클릭 시 위치 갱신
   const handelClickGetmyLocation = async () => {
     try {
       const { data: newLocationData } = await locationRefetch();
@@ -173,6 +173,18 @@ export default function KakaoMap() {
       console.error("데이터 다시 불러오기 실패:", error);
     }
   };
+
+  useEffect(() => {
+    if (mapRef.current && centerCoordinate.x !== 0) {
+      refetchBinData();
+    }
+  }, [centerCoordinate, refetchBinData]);
+
+  useEffect(() => {
+    if (binData && mapRef.current) {
+      updateMarkers(binData, mapRef.current, binkMarkerRef);
+    }
+  }, [binData]);
 
   return (
     <>
